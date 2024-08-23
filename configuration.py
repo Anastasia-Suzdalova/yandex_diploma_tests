2266707754bca12eb124c85485d677e2ed51c958
Suzdalova Anastasia <suzdalovanastasiya@gmail.com>
2024-08-24 00:25:10 +0500

    add screen and corrections in order_test.py

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-URL_SERVICE = "https://56175e59-b927-4971-89a8-3daec324f5d1.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://4f35b108-dc3c-46e7-9f01-edc717e35fa4.serverhub.praktikum-services.ru"
 
 CREATE_ORDER = "/api/v1/orders"
 
